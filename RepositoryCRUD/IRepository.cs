77a4c4b80ee6727a51b874aadfcd509776aa0018
Yevhen <buhaienkoyevhen@gmail.com>
2021-04-08 15:05:00 +0300

    Подключаем сервер и создаем базу данных если она еще не создана. Название базы передаем в конструктор репозитория.

---
+++ RepositoryCRUD/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryCRUD
+{
+    /*
+     Задание 1: 
+Описать интерфейс IRepository<T>, который будет инкапсулировать в себе CRUD (create-read-update-delete) операции с данными:
+•	получение всех данных
+•	получение элемента по Id
+•	Добавление элемента
+•	Изменение элемента
+•	Удаление элемента
+
+    getting all data • getting an element by Id • Adding an element • Changing an element • Deleting an element
+     */
+    interface IRepository<T>
+    {
+        List<T> GetAllData();
+        T GetElementById(int id);
+        void AddElement(T element);
+        void ChangeElement(T element);
+        void DeleteElement(int id);
+    }
+}
---
+++ RepositoryCRUD/MyElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryCRUD
+{
+    class MyElement
+    {
+        public MyElement()
+        {
+            Count++;
+            Name = $"MyElement {Count}";
+        }
+
+        public MyElement(int id):this()
+        {
+            Id = id;
+           
+        }
+
+        static int Count { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
