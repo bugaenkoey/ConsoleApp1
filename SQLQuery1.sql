b5d6896b785b180a9f91782cfba5e71bf4e9d637
Yevhen <buhaienkoyevhen@gmail.com>
2021-05-06 09:51:02 +0300

    Выполнено

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿
+/* Показать среднее количество товаров по каждому типу товара.", 
+$"select * {fromProducts} WHERE Count = ( select  AVG(Count)  {fromProducts} )")
+
+
+select * from Products WHERE Count = ( select  AVG(Count)  from Products )
+*/
+select Distinct Type,AVG(Count)  from Products GROUP BY Type;
+
+select* from Products
